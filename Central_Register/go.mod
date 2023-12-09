b37a3fc954cffafdd9609840967961a2fcbd5ad7
jculley01 <113144839+jculley01@users.noreply.github.com>
2023-12-09 00:57:35 -0500

    Add files via upload

--- Central_Register/go.mod
+++ Central_Register/go.mod
@@ -4,14 +4,20 @@ go 1.21
 
 require (
 	github.com/gorilla/websocket v1.5.1
+	github.com/influxdata/influxdb-client-go/v2 v2.13.0
+	github.com/shirou/gopsutil v3.21.11+incompatible
 	google.golang.org/protobuf v1.31.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	github.com/influxdata/line-protocol v0.0.0-20200327222509-2487e7298839 // indirect
+	github.com/oapi-codegen/runtime v1.0.0 // indirect
+	github.com/tklauser/go-sysconf v0.3.13 // indirect
+	github.com/tklauser/numcpus v0.7.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
-	google.golang.org/grpc v1.59.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
 )
--- Central_Register/main.go
+++ Central_Register/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	gs "centralReg/grpc_status"
 	pb "centralReg/service_reg"
 	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/websocket"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/disk"
+	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/net"
 	"log"
 	"net/http"
 	"os"
@@ -16,8 +18,27 @@ import (
 	"time"
 )
 
-type ApiService = pb.APIService
-type Registration = pb.Registration
+type SystemMetrics struct {
+	CPUUsage    []float64
+	MemoryUsage *mem.VirtualMemoryStat
+	DiskUsage   []disk.UsageStat
+	NetIO       []net.IOCountersStat
+}
+
+type Metrics struct {
+	InfluxDBURL string                 `json:"influxdb_url"`
+	Token       string                 `json:"token"`
+	Org         string                 `json:"org"`
+	Bucket      string                 `json:"bucket"`
+	Measurement string                 `json:"measurement"`
+	Tags        map[string]string      `json:"tags"`
+	Fields      map[string]interface{} `json:"fields"`
+}
+
+type ApiService struct {
+	pb.APIService
+	WSConn *websocket.Conn // WebSocket connection for REST services
+}
 
 var registry = struct {
 	sync.RWMutex
@@ -31,88 +52,112 @@ var upgrader = websocket.Upgrader{
 
 func main() {
 	go monitorServices()
+	setupEndpoints()
 
-	// Use the PORT environment variable
+	clientWrapperSystem := NewClientWrapper("http://35.236.200.122:8086/", "CentralReg", "API-Observability", "AxNHAn8hBBhsHz0o6HVJ2iM9gfGqybVWugTx5crw0o2yvkPTURsZqztPjxOXp4YWR2Hy9jiQPZePyilXFh7lcg==")
+	defer clientWrapperSystem.client.Close()
+
+	// Run metric collection in a separate goroutine
+	go startMetricCollection(clientWrapperSystem, 15*time.Second)
+
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8090" // Default to 8090 if PORT is not set
+		port = "8090"
 	}
 
+	log.Printf("Service Registry is running on port %s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func startMetricCollection(clientWrapper *ClientWrapper, interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		metrics, err := collectSystemMetrics()
+		if err != nil {
+			fmt.Println("Error collecting system metrics:", err)
+			continue
+		}
+
+		if err := clientWrapper.WriteMetric("system_metrics", nil, metrics); err != nil {
+			fmt.Println("Error writing to InfluxDB:", err)
+		}
+	}
+}
+
+func collectSystemMetrics() (map[string]interface{}, error) {
+	var metrics = make(map[string]interface{})
+
+	// Collect CPU usage
+	cpuUsage, err := cpu.Percent(time.Second, false) // false to get aggregated percentage across all CPUs
+	if err != nil {
+		return nil, fmt.Errorf("error collecting CPU metrics: %w", err)
+	}
+	if len(cpuUsage) > 0 {
+		metrics["cpu_usage"] = cpuUsage[0] // Assuming you want the aggregated CPU usage
+	}
+
+	// Collect Memory usage
+	vmStat, err := mem.VirtualMemory()
+	if err != nil {
+		return nil, fmt.Errorf("error collecting memory metrics: %w", err)
+	}
+	metrics["memory_total"] = vmStat.Total
+	metrics["memory_used"] = vmStat.Used
+	metrics["memory_free"] = vmStat.Free
+
+	// Collect Disk usage
+	// Assuming you want to collect data of the root partition '/'
+	diskStat, err := disk.Usage("/")
+	if err != nil {
+		return nil, fmt.Errorf("error collecting disk metrics: %w", err)
+	}
+	metrics["disk_total"] = diskStat.Total
+	metrics["disk_used"] = diskStat.Used
+	metrics["disk_free"] = diskStat.Free
+
+	// Collect Network I/O stats
+	netIO, err := net.IOCounters(false) // false to get stats summed across all network interfaces
+	if err != nil {
+		return nil, fmt.Errorf("error collecting network IO metrics: %w", err)
+	}
+	if len(netIO) > 0 {
+		metrics["net_bytes_sent"] = netIO[0].BytesSent
+		metrics["net_bytes_recv"] = netIO[0].BytesRecv
+		metrics["net_packets_sent"] = netIO[0].PacketsSent
+		metrics["net_packets_recv"] = netIO[0].PacketsRecv
+	}
+
+	return metrics, nil
+}
+
+func setupEndpoints() {
 	http.HandleFunc("/register", registerService)
 	http.HandleFunc("/services", listServices)
-
-	fmt.Printf("Service Registry is running on port %s\n", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
-	}
+	http.HandleFunc("/metrics", metricsEndpoint)
 }
 
 func monitorServices() {
+	ticker := time.NewTicker(15 * time.Second)
+	defer ticker.Stop()
+
 	for {
+		<-ticker.C
 		registry.RLock()
 		for _, service := range registry.services {
-			//go checkServiceStatus(service)
-			if service.Type == "gRPC" {
-				go checkGRPCServiceStatus(service)
-			} else {
-				go checkRESTServiceStatus(service)
+			if service.WSConn != nil {
+				err := service.WSConn.WriteMessage(websocket.PingMessage, nil)
+				if err != nil {
+					log.Printf("Failed to send heartbeat to service %s: %v", service.Name, err)
+					service.Status = "Down"
+				} else {
+					service.Status = "Up"
+					log.Printf("Successful heartbeat to service %s", service.Name)
+				}
 			}
-
 		}
 		registry.RUnlock()
-		time.Sleep(10 * time.Second) // Check every 10 seconds
-	}
-}
-
-func checkRESTServiceStatus(service *ApiService) {
-	var url string
-	if service.Host == "localhost" {
-		url = fmt.Sprintf("http://%s:%d/status", service.Host, service.Port)
-	} else {
-		url = fmt.Sprintf("https://%s/status", service.Host)
-	}
-
-	// Print the URL to the console
-	fmt.Println("Checking service status at:", url)
-
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode != http.StatusOK {
-		service.Status = "Down"
-	} else {
-		service.Status = "Up"
-	}
-
-	// It's important to close the response body to free resources
-	if resp != nil {
-		resp.Body.Close()
-	}
-}
-
-func checkGRPCServiceStatus(service *ApiService) {
-	// Set up a connection to the server with insecure credentials for simplicity
-	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", service.Host, service.Port), grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Printf("Failed to dial gRPC service: %v", err)
-		service.Status = "Down"
-		return
-	}
-	defer conn.Close()
-
-	// Create a new StatusService client
-	client := gs.NewStatusServiceClient(conn)
-
-	// Prepare a context with timeout
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
-
-	// Call the CheckStatus method
-	response, err := client.CheckStatus(ctx, &gs.StatusRequest{})
-	if err != nil {
-		log.Printf("Error calling CheckStatus: %v", err)
-		service.Status = "Down"
-	} else {
-		service.Status = response.Status
-		//service.Status = "Up"
 	}
 }
 
@@ -122,43 +167,33 @@ func registerService(w http.ResponseWriter, r *http.Request) {
 		log.Println("Upgrade to websocket failed:", err)
 		return
 	}
-	defer conn.Close()
 
-	for {
-		// Read message from WebSocket connection
-		_, msg, err := conn.ReadMessage()
-		if err != nil {
-			log.Println("Error reading message:", err)
-			break
-		}
-
-		var reg Registration
-		if err := json.Unmarshal(msg, &reg); err != nil {
-			log.Printf("Error decoding registration data: %s", err)
-			conn.WriteMessage(websocket.TextMessage, []byte("Invalid registration data"))
-			continue
-		}
-
-		registry.Lock()
-		if existingService, exists := registry.services[reg.Name]; exists {
-			// Optionally update existing registration instead of ignoring
-			existingService.Host = reg.Host
-			existingService.Port = reg.Port
-			existingService.Type = reg.Type
-			log.Printf("Updated registration for service %s", reg.Name)
-		} else {
-			registry.services[reg.Name] = &ApiService{
-				Name: reg.Name,
-				Host: reg.Host,
-				Port: reg.Port,
-				Type: reg.Type,
-			}
-			log.Printf("Service %s registered successfully with Host: %s, Port: %d", reg.Name, reg.Host, reg.Port)
-		}
-		registry.Unlock()
-
-		conn.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("Service %s processed", reg.Name)))
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		log.Println("Error reading registration message:", err)
+		return
 	}
+
+	var reg pb.Registration
+	if err := json.Unmarshal(msg, &reg); err != nil {
+		log.Printf("Error decoding registration data: %s", err)
+		return
+	}
+
+	registry.Lock()
+	if existingService, exists := registry.services[reg.Name]; exists {
+		existingService.Type = reg.Type
+		existingService.WSConn = conn
+	} else {
+		registry.services[reg.Name] = &ApiService{
+			APIService: pb.APIService{
+				Name: reg.Name,
+				Type: reg.Type,
+			},
+			WSConn: conn,
+		}
+	}
+	registry.Unlock()
 }
 
 func listServices(w http.ResponseWriter, r *http.Request) {
@@ -173,17 +208,74 @@ func listServices(w http.ResponseWriter, r *http.Request) {
 	defer registry.RUnlock()
 
 	services := make([]*ApiService, 0, len(registry.services))
-
 	for _, service := range registry.services {
 		services = append(services, service)
 	}
 
-	// Send the service list over WebSocket
 	err = conn.WriteJSON(services)
 	if err != nil {
 		log.Println("Error sending services over WebSocket:", err)
+	}
+}
+
+func metricsEndpoint(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade to websocket failed:", err)
 		return
 	}
+	defer conn.Close()
 
-	log.Println("Sent services list over WebSocket")
+	handleMetrics(conn)
+}
+
+func handleMetrics(conn *websocket.Conn) {
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading metrics message:", err)
+			break
+		}
+
+		var metrics Metrics
+		if err := json.Unmarshal(msg, &metrics); err != nil {
+			log.Printf("Error decoding metrics data: %s", err)
+			continue
+		}
+
+		if err := sendToInfluxDB(metrics); err != nil {
+			log.Printf("Error sending to InfluxDB: %s", err)
+		}
+	}
+}
+
+func sendToInfluxDB(metrics Metrics) error {
+	clientWrapper := NewClientWrapper(metrics.InfluxDBURL, metrics.Bucket, metrics.Org, metrics.Token)
+	defer clientWrapper.client.Close()
+	err := clientWrapper.WriteMetric(metrics.Measurement, metrics.Tags, metrics.Fields)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type ClientWrapper struct {
+	client influxdb2.Client
+	org    string
+	bucket string
+}
+
+func NewClientWrapper(url, bucket, org, token string) *ClientWrapper {
+	client := influxdb2.NewClient(url, token)
+	return &ClientWrapper{
+		client: client,
+		org:    org,
+		bucket: bucket,
+	}
+}
+
+func (cw *ClientWrapper) WriteMetric(measurement string, tags map[string]string, fields map[string]interface{}) error {
+	writeAPI := cw.client.WriteAPIBlocking(cw.org, cw.bucket)
+	p := influxdb2.NewPoint(measurement, tags, fields, time.Now())
+	return writeAPI.WritePoint(context.Background(), p)
 }
